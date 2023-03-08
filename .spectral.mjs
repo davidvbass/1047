b30b3a5fc8b2dab56de8c2934c3dd91e356c3032
myspacecfo@gmail.com <myspacecfo@gmail.com>
2023-03-08 12:42:49 -0800

    Modified 17 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight-local.com:8443/api/v1/projects/cHJqOjIz/spectral.js?branch_slug=master&token=31985397-11ae-41b9-b5ea-16771670390a";
+export default { extends: ruleset };
